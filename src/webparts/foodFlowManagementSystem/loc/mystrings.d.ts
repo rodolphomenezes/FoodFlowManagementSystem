b1f9513ff15af9e692af4fcef79565737dc10a5b
Rodolpho Menezes <rodolphomenezes@yahoo.com.br>
2023-06-28 02:21:51 +0100

    First commit

---
+++ src/webparts/foodFlowManagementSystem/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IFoodFlowManagementSystemWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'FoodFlowManagementSystemWebPartStrings' {
+  const strings: IFoodFlowManagementSystemWebPartStrings;
+  export = strings;
+}
